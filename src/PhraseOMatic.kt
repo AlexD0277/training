5653a0b382cd6a3297edd537b0a618bebd0c7aa6
AlexD0277 <derepaskoalex@gmail.com>
2021-11-30 15:19:48 +1000

    ij

---
+++ src/PhraseOMatic.kt
@@ -0,0 +1,14 @@
+import java.lang.Math.random
+
+fun main(args: Array<String>){
+    val fruit = arrayOf("Apple", "Banana", "Cherry", "Blueberry", "Pomegranate")
+    val index = arrayOf(1, 3, 4, 2)
+    var y: Int
+    var x = 0
+    while (x < 4) {
+        y = index[x]
+        println("Fruit = ${fruit[y]}")
+        x=x+1
+        //ljlkjlkj
+    }
+}
