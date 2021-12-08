d304310613f69cbadd4bb37702f7a247058ca77e
Professional <derepasko@list.ru>
2021-12-08 22:13:20 +1000

    ij

--- RPS/src/RPS.kt
+++ RPS/src/RPS.kt
@@ -1,6 +1,23 @@
 fun getGameChoise(optionsParam:Array<String>)= optionsParam[(Math.random()*optionsParam.size).toInt()]
+fun getUserChoise(optionsParam:Array<String>):String{
+    var isValidChoise=false
+    var userChoise =""
+    while(!isValidChoise){
+        print("Please enter one of the following: ")
+        for(item in optionsParam)print(" $item")
+        println(".")
+        var userInput = readLine()
+        if(userInput!=null&& userInput in optionsParam){
+            isValidChoise=true
+            userChoise = userInput
+        }
+        else println("enter valid choise")
+    }
+    return userChoise
+}
 fun main() {
     val options = arrayOf("Rock","Paper", "Scissors")
     val gameChoice = getGameChoise(options)
+    val userChoise = getUserChoise(options)
     //110
 }
