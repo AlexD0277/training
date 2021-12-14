2d8929f4e5242f0329874eff4838c806733f1247
Professional <derepasko@list.ru>
2021-12-14 22:03:08 +1000

    ij

--- RPS/src/Songs.kt
+++ RPS/src/Songs.kt
@@ -1,3 +1,16 @@
+class Song(val title:String, val artist: String){
+    fun Play(){
+        println("Playing the song $title by $artist")
+    }
+    fun Stop(){
+        println("Stopped playing $title")
+    }
+}
 fun main(){
-
+    val song1 = Song("Айсберг","Алла Пугачева")
+    val song2 = Song("Ромашки","Земфира")
+    val song3 = Song("Black and White","Michel Jackson")
+    song2.Play()
+    song2.Stop()
+    song3.Play()
 }
