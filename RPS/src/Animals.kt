c3755a21d1b63b9c2bfbba7805f0186ba9e30b92
AlexD0277 <derepaskoalex@gmail.com>
2021-12-21 12:35:51 +1000

    Merge remote-tracking branch 'origin/RockPaperScissors' into RockPaperScissors
    # Conflicts:
    #	RPS/src/RPS.kt

--- RPS/src/Animals.kt
+++ RPS/src/Animals.kt
@@ -47,5 +47,24 @@ class Wolf:Canine(){
     override fun eat() {
         println("The wolf is eating $food")
     }
-    //175
 }
+class Vet(){
+    fun giveShot(animal:Animal){
+        animal.makeNoise()
+    }
+}
+
+fun main() {
+    val animals= arrayOf(Hippo(),Wolf())
+    for(item in animals) {
+        item.roam()
+        item.eat()
+    }
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+}
+
+//175
