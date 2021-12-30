a42c1ac3bd457ead4e240a2baaa671484e8df6af
AlexD0277 <derepaskoalex@gmail.com>
2021-12-30 13:55:09 +1000

    Merge remote-tracking branch 'origin/RockPaperScissors' into RockPaperScissors
    # Conflicts:
    #	RPS/src/RPS.kt

--- RPS/src/test.kt
+++ RPS/src/test.kt
@@ -1,19 +1,28 @@
 fun main() {
-    val petsLiam = listOf("Cat", "Dog", "Fish", "Fish")
-    val petsSophia = listOf("Cat", "Owl")
-    val petsNoah = listOf("Dog", "Dove", "Dog", "Dove")
-    val petsEmily = listOf("Hedgehog")
-    var pets = mutableListOf<String>()
-    pets.addAll(petsLiam)
-    pets.addAll(petsEmily)
-    pets.addAll(petsNoah)
-    pets.addAll(petsSophia)
-    println(pets)
-    println(pets.size)
-    pets.sort()
-    println(pets.toSet())
+    val term1 = "Array"
+    val term2 = "List"
+    val term3 = "Map"
+    val term4 = "MutableList"
+    val term5 = "MutableMap"
+    val term6 = "MutableSet"
+    val term7 = "Set"
+    val def1 = "Holds values in no particular order."
+    val def2 = "Holds key/value pairs."
+    val def3 = "Holds values in a sequence."
+    val def4 = "Can be updated."
+    val def5 = "Can't be updated."
+    val def6 = "Can be resized."
+    val def7 = "Can't be resized."
+
+    val glossary = mapOf(
+        term4 to "$def3 $def4 $def6",
+        term7 to "$def1 $def5 $def7",
+        term1 to "$def3 $def4 $def7",
+        term5 to "$def2 $def4 $def6",
+        term2 to "$def3 $def5 $def7",
+        term6 to "$def1 $def4 $def6",
+        term3 to "$def2 $def5 $def7"
+    )
+    for ((key, value) in glossary) println("$key: $value")
 }
-
-
-
-//281
+//320
